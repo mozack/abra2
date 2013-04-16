c761d157a076f496bfa0cd8e25deb79e5953c452
Lisle Mose <lislemose@gmail.com>
2013-04-16 11:27:01 -0400

    Missed previous checkin.

---
+++ src/main/java/abra/ReadPair.java
@@ -0,0 +1,54 @@
+package abra;
+
+import net.sf.samtools.SAMRecord;
+
+/**
+ * A Pair of SAMRecord's
+ * 
+ * @author Lisle Mose (lmose at unc dot edu)
+ */
+public class ReadPair {
+    private SAMRecord read1;
+    private SAMRecord read2;
+    
+    private String hashString = null;
+    
+    ReadPair(SAMRecord read1, SAMRecord read2) {
+        this.read1 = read1;
+        this.read2 = read2;
+    }
+    
+    public SAMRecord getRead1() {
+        return read1;
+    }
+    
+    public SAMRecord getRead2() {
+        return read2;
+    }
+    
+    public String toString() {
+        String r1 = read1 != null ? read1.getReadName() : "null";
+        String r2 = read2 != null ? read2.getReadName() : "null";
+        return "read1: " + r1 + ", read2: " + r2;
+    }
+    
+    private synchronized String getHashString() {
+    	if (hashString == null) {
+    		hashString = read1 != null ? read1.getSAMString() : "null" +
+    					 read2 != null ? read2.getSAMString() : "null";
+    	}
+    	
+    	return hashString;
+    }
+    
+    @Override
+    public int hashCode() {
+    	return getHashString().hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+    	ReadPair that = (ReadPair) obj;
+    	return this.getHashString().equals(that.getHashString());
+    }
+}

5b5814fa1dcbd24a5f5cb8e52d30a7fd34f72827
Lisle Mose <lislemose@gmail.com>
2013-06-25 16:03:08 -0400

    Log version as 44b

--- src/main/java/abra/ReAligner.java
+++ src/main/java/abra/ReAligner.java
@@ -1758,7 +1758,7 @@ public class ReAligner {
 
 	public static void run(String[] args) throws Exception {
 		
-		System.out.println("Starting 0.44 ...");
+		System.out.println("Starting 0.44b ...");
 		
 		ReAlignerOptions options = new ReAlignerOptions();
 		options.parseOptions(args);

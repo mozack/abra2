5a45b7d721ef1f3494bd80f026e2e80978abbd49
Lisle Mose <lislemose@gmail.com>
2013-08-23 13:17:43 -0400

    Added more detail to multi-map error message.

--- src/main/java/abra/SimpleSamReadPairReader.java
+++ src/main/java/abra/SimpleSamReadPairReader.java
@@ -46,7 +46,7 @@ public class SimpleSamReadPairReader implements Iterable<ReadPair> {
     private ReadPair pairReads(List<SAMRecord> reads1, List<SAMRecord> reads2) {
     	
     	if ((reads1.size() > 1) || (reads2.size() > 1)) {
-    		String msg = "Error: Multi-mapped reads not supported.";
+    		String msg = "Error: Multi-mapped reads not supported: " + reads1.get(0).getReadName() + " - " + reads2.get(0).getReadName();
     		System.out.println(msg);
     		throw new RuntimeException(msg);
     	}

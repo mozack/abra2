20ebb2d2ffbabb60ff753a74de24fb26bcadb568
Lisle Mose <lislemose@gmail.com>
2014-01-17 06:45:57 -0500

    Improved hash function.

--- src/main/c/assembler.c
+++ src/main/c/assembler.c
@@ -39,6 +39,52 @@ __thread int kmer_size;
 __thread int min_node_freq;
 __thread int min_base_quality;
 
+#define BIG_CONSTANT(x) (x##LLU)
+
+uint64_t MurmurHash64A ( const void * key, int len, uint64_t seed )
+{
+  const uint64_t m = BIG_CONSTANT(0xc6a4a7935bd1e995);
+  const int r = 47;
+
+  uint64_t h = seed ^ (len * m);
+
+  const uint64_t * data = (const uint64_t *)key;
+  const uint64_t * end = data + (len/8);
+
+  while(data != end)
+  {
+    uint64_t k = *data++;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+
+    h ^= k;
+    h *= m;
+  }
+
+  const unsigned char * data2 = (const unsigned char*)data;
+
+  switch(len & 7)
+  {
+  case 7: h ^= uint64_t(data2[6]) << 48;
+  case 6: h ^= uint64_t(data2[5]) << 40;
+  case 5: h ^= uint64_t(data2[4]) << 32;
+  case 4: h ^= uint64_t(data2[3]) << 24;
+  case 3: h ^= uint64_t(data2[2]) << 16;
+  case 2: h ^= uint64_t(data2[1]) << 8;
+  case 1: h ^= uint64_t(data2[0]);
+          h *= m;
+  };
+
+  h ^= h >> r;
+  h *= m;
+  h ^= h >> r;
+
+  return h;
+}
+
+
 struct eqstr
 {
   bool operator()(const char* s1, const char* s2) const
@@ -49,22 +95,31 @@ struct eqstr
 
 struct my_hash
 {
-	unsigned long operator()(const char* kmer) const
+	uint64_t operator()(const char* kmer) const
 	{
-		unsigned long hash = 0;
-		int c;
-
-//		while((c = *kmer++))
-		for (int i=0; i<kmer_size; i++)
-		{
-			c = kmer[i];
-			/* hash = hash * 33 ^ c */
-			hash = ((hash << 5) + hash) ^ c;
-		}
-
-		return hash;
+		return MurmurHash64A(kmer, kmer_size, 97);
+		//return chunk;
 	}
 };
+
+//struct my_hash
+//{
+//	unsigned long operator()(const char* kmer) const
+//	{
+//		unsigned long hash = 0;
+//		int c;
+//
+////		while((c = *kmer++))
+//		for (int i=0; i<kmer_size; i++)
+//		{
+//			c = kmer[i];
+//			/* hash = hash * 33 ^ c */
+//			hash = ((hash << 5) + hash) ^ c;
+//		}
+//
+//		return hash;
+//	}
+//};
 
 struct struct_pool {
 	struct node_pool* node_pool;

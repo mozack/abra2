904e257fd57216073af408f78dd338617d1c2d09
Lisle Mose <lislemose@gmail.com>
2014-01-31 13:06:00 -0500

    Missed adding this in prev commit.

---
+++ src/main/java/abra/AbraRunnable.java
@@ -0,0 +1,29 @@
+package abra;
+
+/**
+ * Abstract base class for ABRA threads.
+ * 
+ * @author Lisle E. Mose (lmose at unc dot edu)
+ */
+public abstract class AbraRunnable implements Runnable {
+
+	private ThreadManager threadManager;
+	
+	public AbraRunnable(ThreadManager threadManager) {
+		this.threadManager = threadManager;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			go();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		} finally {
+			threadManager.removeThread(this);
+		}
+	}
+	
+	public abstract void go() throws Exception;
+}

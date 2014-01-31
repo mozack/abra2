b235896115d873657b56224c841402de28463667
Lisle Mose <lislemose@gmail.com>
2014-01-30 21:05:34 -0500

    Upped to v0.70

--- src/main/java/abra/ReAligner.java
+++ src/main/java/abra/ReAligner.java
@@ -1299,7 +1299,7 @@ public class ReAligner {
 
 	public static void run(String[] args) throws Exception {
 		
-		System.out.println("Starting 0.69 ...");
+		System.out.println("Starting 0.70 ...");
 		
 		ReAlignerOptions options = new ReAlignerOptions();
 		options.parseOptions(args);

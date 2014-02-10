b801a780cf433c1c48df4eb6ad41f775cdfa683b
Lisle Mose <lislemose@gmail.com>
2014-02-10 18:42:16 -0500

    Set version to 0.72

--- src/main/java/abra/ReAligner.java
+++ src/main/java/abra/ReAligner.java
@@ -1092,7 +1092,7 @@ public class ReAligner {
 
 	public static void run(String[] args) throws Exception {
 		
-		System.out.println("Starting 0.71 ...");
+		System.out.println("Starting 0.72 ...");
 		
 		ReAlignerOptions options = new ReAlignerOptions();
 		options.parseOptions(args);

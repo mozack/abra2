a201b2d652c11e23079f7d61b978905519648930
Lisle Mose <lislemose@gmail.com>
2014-04-28 15:12:44 -0400

    Modified demo script to write output to log file.

--- demo/demo.bash
+++ demo/demo.bash
@@ -1,5 +1,6 @@
 # Demonstration command line for ABRA.
 # bwa mem and a recent version of java must be in your command path
+# Assumes your current working directory is the demo directory
 
 # Path to ABRA jar file (with dependencies)
 # If you've downloaded the jar, set this to the appropriate location
@@ -10,4 +11,4 @@ JAR=../target/abra-0.75-SNAPSHOT-jar-with-dependencies.jar
 #REF=/datastore/rclbg/nextgenout3/MOSE_TEST/abra/brca/ref/GRCh37-lite.fa
 REF=<path to hg19 reference>
 
-java -Xmx4G -jar $JAR --ref $REF --in abra_demo.bam --out abra_demo_realigned.bam --kmer 43,53,63,73,83 --working abra_temp_dir --targets demo.bed
+java -Xmx4G -jar $JAR --ref $REF --in abra_demo.bam --out abra_demo_realigned.bam --kmer 43,53,63,73,83 --working abra_temp_dir --targets demo.bed > abra_demo.log 2>&1

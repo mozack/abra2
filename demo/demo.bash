7950fa75ec4a72022107785964890feffdd80a4e
Lisle Mose <lislemose@gmail.com>
2014-04-28 14:27:05 -0400

    Adding more comments to demo.

--- demo/demo.bash
+++ demo/demo.bash
@@ -1,9 +1,12 @@
+# Demonstration command line for ABRA.
+# bwa mem and a recent version of java must be in your command path
+
 # Path to ABRA jar file (with dependencies)
 # If you've downloaded the jar, set this to the appropriate location
 # Compiled jars will be under the target directory
 JAR=../target/abra-0.75-SNAPSHOT-jar-with-dependencies.jar
 
-# Path to hg19 reference (indexed by BWA.  bwa mem must be in your command path)
+# Path to hg19 reference (indexed by BWA)
 #REF=/datastore/rclbg/nextgenout3/MOSE_TEST/abra/brca/ref/GRCh37-lite.fa
 REF=<path to hg19 reference>
 

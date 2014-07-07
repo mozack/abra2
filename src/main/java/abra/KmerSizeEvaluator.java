3ffaad84f941001d9acfbd7b14f8b6e3dbcb0f15
Lisle Mose <lislemose@gmail.com>
2014-07-07 14:33:12 -0400

    Allow ref map to be passed in.  Accomodate 2 bit map (N's may not be accurately identified).  Expose kmer evaluation for multiple regions to other classes.

--- src/main/java/abra/KmerSizeEvaluator.java
+++ src/main/java/abra/KmerSizeEvaluator.java
@@ -22,28 +22,27 @@ public class KmerSizeEvaluator {
 	public static final int MIN_KMER = 5;
 
 	private int readLength;
-	private String reference;
+	private CompareToReference2 c2r;
 	private String outputFile;
 	private BufferedWriter output;
 	private Set<Feature> outputRegions;
 	private ThreadManager threadManager;
 	private String regionsBed;
 	
-	public KmerSizeEvaluator(int readLength, String reference, String outputFile, int numThreads, String regionsBed) {
+	public KmerSizeEvaluator(int readLength, CompareToReference2 c2r, String outputFile, int numThreads, String regionsBed) {
 		this.readLength = readLength;
-		this.reference = reference;
+		this.c2r = c2r;
 		this.outputFile = outputFile;
 		this.regionsBed = regionsBed;
-		
 		this.threadManager = new ThreadManager(numThreads);
+	}
+	
+	public KmerSizeEvaluator() {
 	}
 		
 	public void run() throws IOException, InterruptedException {
 				
-//		new NativeLibraryLoader().load(".");
-		CompareToReference2 c2r = new CompareToReference2();
-		c2r.init8bit(reference);
-		
+//		new NativeLibraryLoader().load(".");		
 		List<Feature> regions = ReAligner.getRegions(regionsBed, readLength);
 		
 		output = new BufferedWriter(new FileWriter(outputFile, false));
@@ -87,7 +86,13 @@ public class KmerSizeEvaluator {
 		region.setAdditionalInfo((readLength+1) + "\tN");
 	}
 	
-	private void evalRegion(Feature region, String regionBases) {	
+	public int identifyMinKmer(int readLength, CompareToReference2 c2r, List<Feature> regions) {
+		List<String> regionBases = new ArrayList<String>();
+		
+		for (Feature region : regions) {
+			regionBases.add(c2r.getSequence(region.getSeqname(), (int) region.getStart()+1-(readLength-1), (int) region.getLength() + (readLength-1)));
+		}
+		
 		boolean isEditDistanceOK = false;
 		int distKmer = MIN_KMER;
 		while (!isEditDistanceOK && distKmer < readLength) {
@@ -97,6 +102,14 @@ public class KmerSizeEvaluator {
 			}
 		}
 
+		return distKmer;
+	}
+	
+	private void evalRegion(Feature region, String regionBases) {	
+
+		List<Feature> regionList = new ArrayList<Feature>();
+		int distKmer = identifyMinKmer(readLength, c2r, regionList);
+		
 		region.setAdditionalInfo(String.valueOf(distKmer) + "\t.");
 				
 		outputRegions.add(region);
@@ -167,19 +180,22 @@ public class KmerSizeEvaluator {
 	}
 
 	// Is the hamming distance between all bases in this region at least 2
-	private boolean isHammingDistanceAtLeast2(String bases, int k) {
+	private boolean isHammingDistanceAtLeast2(List<String> basesList, int k) {
 		
 		List<String> kmers = new ArrayList<String>();
-		for (int i=0; i<=bases.length()-k; i++) {
-			String kmer = bases.substring(i, i+k);
-			
-			for (String kmer2 : kmers) {
-				if (!isHammingDistanceAtLeast2(kmer, kmer2)) {
-					return false;
+		
+		for (String bases : basesList) {
+			for (int i=0; i<=bases.length()-k; i++) {
+				String kmer = bases.substring(i, i+k);
+				
+				for (String kmer2 : kmers) {
+					if (!isHammingDistanceAtLeast2(kmer, kmer2)) {
+						return false;
+					}
 				}
+				
+				kmers.add(kmer);
 			}
-			
-			kmers.add(kmer);
 		}
 		
 		return true;
@@ -227,7 +243,10 @@ public class KmerSizeEvaluator {
 		String tempDir = args[5];
 
 		double s = System.currentTimeMillis();
-		KmerSizeEvaluator re = new KmerSizeEvaluator(readLength, reference, outputBed, threads, regionsBed);
+		CompareToReference2 c2r = new CompareToReference2();
+		c2r.init(reference);
+		
+		KmerSizeEvaluator re = new KmerSizeEvaluator(readLength, c2r, outputBed, threads, regionsBed);
 		re.init(tempDir);
 		re.run();
 		double e = System.currentTimeMillis();

29c339edca3cfeed038c746837f48cf6a8660846
Lisle Mose <lislemose@gmail.com>
2014-07-15 12:18:25 -0400

    Temporarily disabling ambiguous read check.

--- src/main/java/abra/Sam2Fastq.java
+++ src/main/java/abra/Sam2Fastq.java
@@ -64,7 +64,9 @@ public class Sam2Fastq {
     			
     			int yx = 0;
     			
-    			boolean isAmbiguous = !read.getReadUnmappedFlag() && read.getMappingQuality() == 0; 
+//    			boolean isAmbiguous = !read.getReadUnmappedFlag() && read.getMappingQuality() == 0;
+    			
+    			boolean isAmbiguous = false;
     			
     			if ((!read.getReadFailsVendorQualityCheckFlag()) && (!isAmbiguous)) {
 	    			// Calculate the number of mismatches to reference for this read.

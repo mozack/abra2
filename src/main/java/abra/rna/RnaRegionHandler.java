94ea47bf5ca8592e819eac6fb2749d9a3997f1ae
Lisle Mose <lislemose@gmail.com>
2014-10-09 11:06:11 -0400

    Thread logging.

--- src/main/java/abra/rna/RnaRegionHandler.java
+++ src/main/java/abra/rna/RnaRegionHandler.java
@@ -10,16 +10,22 @@ public class RnaRegionHandler extends AbraRunnable {
 	
 	private RnaPoc poc;
 	private List<SAMRecord> reads;
+	private String id;
 
 	public RnaRegionHandler(ThreadManager threadManager, RnaPoc poc, List<SAMRecord> reads) {
 		super(threadManager);
 		this.poc = poc;
 		this.reads = reads;
+		SAMRecord first = reads.get(0);
+		SAMRecord last = reads.get(reads.size()-1);
+		// NOTE: No guarantee that the last read's alignment end is really the end of the region.
+		// Calculating this requires iterating over entire region.
+		id = "thread: " + first.getReferenceName() + "_" + first.getAlignmentStart() + "_" + last.getAlignmentEnd() + " reads: " + reads.size();
 	}
 	
 	@Override
 	public void go() throws Exception {
+		System.out.println("Thread spawned for " + id);
 		poc.processReads(reads);
 	}
-
 }

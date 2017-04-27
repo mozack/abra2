726e003e0f24681ae414120a24015790028762ba
mozack <lisle.mose@gmail.com>
2017-04-27 14:32:32 -0400

    Log assembly retry.

--- src/main/java/abra/ReAligner.java
+++ src/main/java/abra/ReAligner.java
@@ -826,6 +826,7 @@ public class ReAligner {
 							assemblerSettings.getMinEdgeRatio()/2.0);
 					
 					if (shouldRetry) {
+						Logger.info("RETRY_ASSEMBLY: %s", region);
 						// Indel near edge of contig indicates that we may have a low coverage indel encountered.
 						// Try to reassemble using less stringent pruning to see if we can get greater coverage.
 						results.clear();

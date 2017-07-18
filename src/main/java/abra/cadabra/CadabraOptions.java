5e4b230a87ad5b90c5d432eb9caeec688c14086e
mozack <lisle.mose@gmail.com>
2017-07-18 14:46:12 -0400

    Set strp to 5.

--- src/main/java/abra/cadabra/CadabraOptions.java
+++ src/main/java/abra/cadabra/CadabraOptions.java
@@ -50,7 +50,7 @@ public class CadabraOptions extends Options {
             parser.accepts(NORMAL, "Normal BAM file").withRequiredArg().ofType(String.class);
             parser.accepts(SAMPLE, "BAM file to be used for single sample calling").withRequiredArg().ofType(String.class);
             parser.accepts(REFERENCE, "Reference fasta").withRequiredArg().ofType(String.class);
-            parser.accepts(STRP_FILTER, "Filter variants with short tandem repeat period at or above this threshold(-1 to disable)").withRequiredArg().ofType(Integer.class).defaultsTo(6); 
+            parser.accepts(STRP_FILTER, "Filter variants with short tandem repeat period at or above this threshold(-1 to disable)").withRequiredArg().ofType(Integer.class).defaultsTo(5); 
             parser.accepts(HRUN_FILTER, "Filter short indels with a nearby homopolymer run of this length or greater - only neighboring 20 bases searched (-1 to disable)").withRequiredArg().ofType(Integer.class).defaultsTo(6);
             parser.accepts(ISPAN_FILTER, "Filter variants with max index span less than specified value").withRequiredArg().ofType(Integer.class).defaultsTo(20);
             parser.accepts(QUAL_FILTER, "Filter variants with quality score less than specified value").withRequiredArg().ofType(Float.class).defaultsTo(5f);
